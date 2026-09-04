95da2f0c3ed5d63badba4407d69b9c15c886b57a
Vasilev Gregory <g.vasilev@vkteam.ru>
2026-09-04 14:20:15 +0300

    write

---
+++ examples/baremetal/flavor/resource/main.tf
@@ -0,0 +1,30 @@
+data "vkcs_baremetal_flavor" "selected" {
+  name = "BM_CX301_N_BOND"
+}
+
+# Get the current project-specific display name.
+output "display_name_before" {
+  value = data.vkcs_baremetal_flavor.selected.display_name
+}
+
+# Write a project-specific display name.
+resource "vkcs_baremetal_flavor" "main" {
+  id           = data.vkcs_baremetal_flavor.selected.id
+  display_name = "Terraform managed bare metal flavor"
+}
+
+# Rewrite the display name by changing the value above and running:
+# terraform apply
+
+# The data source reads the value written by the resource.
+data "vkcs_baremetal_flavor" "updated" {
+  id = vkcs_baremetal_flavor.main.id
+}
+
+output "display_name" {
+  value = data.vkcs_baremetal_flavor.updated.display_name
+}
+
+# Reset the display name by running:
+# terraform destroy
+# Destroying this resource clears the project-specific display name.
